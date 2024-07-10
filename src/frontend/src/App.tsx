19f4c7a3d763e32013df8dc17b34d360d99b36d9
BGoodes <moullecjulienpro@gmail.com>
2024-07-10 16:21:41 +0800

    Init a React app with Bun

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,17 +1,9 @@
-import {Route, Routes} from "react-router-dom";
-
-import NotFoundPage from "./pages/NotFoundPage.tsx";
-import HomePage from "./pages/HomePage.tsx";
-
-const App = () => {
+function App() {
 
   return (
-      <div className="app">
-          <Routes>
-            <Route index element={<HomePage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-      </div>
+    <>
+      <h1>Hello world!</h1>
+    </>
   )
 }
 
