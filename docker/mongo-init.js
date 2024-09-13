f80cc2214dbab440275d97c4d1bc3e46d4ee9e7d
Marc Lepagneul <marc@lepagneul.fr>
2024-09-13 13:52:52 +0200

    Added collections to db init file

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -1,1 +1,12 @@
+db.createCollection('users');
+db.createCollection('groups');
+db.createCollection('roles');
+
 db.createCollection('notebooks');
+db.createCollection('pages');
+db.createCollection('sheets');
+
+db.createCollection('root_path');
+db.createCollection('path');
+
+
